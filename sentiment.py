ab11275ea40148f60a38e1d1fa38f9b793288bb5
Ayush Singh <46263037+Ayush-Kumar-Singh@users.noreply.github.com>
2022-05-24 14:09:46 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -39,7 +39,7 @@ my_bar = st.progress(0)
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper(title +' since:2021-01-01 until:2021-12-31').get_items()): #declare a username 
     if i>option:
         break
-    st.write(tweet)
+    st.write(tweet,i)
     tcontent.append(tweet.content)
     tusername.append(tweet.user.username)
     tid.append(tweet.id)
