826791edf4f2ef9be6a57579e61c05cded88a455
Ayush Singh <46263037+Ayush-Kumar-Singh@users.noreply.github.com>
2022-05-27 14:04:29 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -119,7 +119,7 @@ Counter = Counter(dictOfWords)
   
 # most_common() produces k frequently encountered
 # input values and their respective counts.
-most_occur = Counter.most_common(4)
+most_occur = Counter.most_common(10)
   
 st.write(most_occur)
 
