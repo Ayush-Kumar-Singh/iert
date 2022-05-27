c34bfa2bedfa019362d239d78a163a7aa86fb2e1
Ayush Singh <46263037+Ayush-Kumar-Singh@users.noreply.github.com>
2022-05-27 13:22:23 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,7 @@
 def custom_legend_name(new_names):
     for i, new_name in enumerate(new_names):
         fig.data[i].name = new_name
+from collections import Counter
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import json
@@ -84,6 +85,18 @@ names = ee
 fig = px.pie(values=random_x, names=names,title=heading)
 
 st.plotly_chart(fig)
+
+# split() returns list of all the words in the string
+split_it = data_set.split()
+  
+# Pass the split_it list to instance of Counter class.
+Counter = Counter(split_it)
+  
+# most_common() produces k frequently encountered
+# input values and their respective counts.
+most_occur = Counter.most_common(4)
+  
+print(most_occur)
 
 
 
